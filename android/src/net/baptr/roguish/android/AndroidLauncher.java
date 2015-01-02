08465052e5f3563e52be6d39de14d47077fbfcc1
Bryce Petrini <baptr01@gmail.com>
2015-01-02 00:11:14 -0800

    Fresh project

---
+++ android/src/net/baptr/roguish/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.baptr.roguish.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.baptr.roguish.Roguish;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Roguish(), config);
+	}
+}
