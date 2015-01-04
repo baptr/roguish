6dcabcf520a1440b2770a27f88f70a96664ccc30
Bryce Petrini <baptr01@gmail.com>
2015-01-04 05:42:30 -0800

    Basic player movement and collision detection. Align player with map.

--- core/src/net/baptr/roguish/Player.java
+++ core/src/net/baptr/roguish/Player.java
@@ -1,24 +1,29 @@
 package net.baptr.roguish;
 
-import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 
-public class Player extends ApplicationAdapter {
+public class Player extends InputAdapter {
 
-  private static final int        CAST_COLS = 7;
-  private static final int        POKE_COLS = 8;
-  private static final int        WALK_COLS = 9;
-  private static final int        FLIP_COLS = 6;
-  private static final int        FIRE_COLS = 13;
-  private static final int        DEAD_COLS = 9;
+  private static final int CAST_COLS = 7;
+  private static final int POKE_COLS = 8;
+  private static final int WALK_COLS = 9;
+  private static final int FLIP_COLS = 6;
+  private static final int FIRE_COLS = 13;
+  private static final int DEAD_COLS = 9;
 
   private static final int DIRECTIONS = 4;
-  private static final int  WALK_OFFSET = 2;
+  private static final int WALK_OFFSET = 2;
+
+  private static final float SPEED = 3;
 
   public enum Direction { UP, LEFT, DOWN, RIGHT };
 
@@ -29,10 +34,14 @@ public class Player extends ApplicationAdapter {
   TextureRegion                   currentFrame;
 
   float stateTime;
+  Vector2 v, iv;
   float x, y;
   Direction dir;
+  Sprite sprite;
 
-  public Player() {
+  boolean[][] colMap;
+
+  public Player(int _x, int _y) {
     walkSheet = new Texture(Gdx.files.internal("lady48.png"));
     TextureRegion[][] tmp = TextureRegion.split(walkSheet, 48, 48);
     walkFrames = new TextureRegion[DIRECTIONS][WALK_COLS];
@@ -45,17 +54,92 @@ public class Player extends ApplicationAdapter {
     }
     spriteBatch = new SpriteBatch();
     stateTime = 0f;
-    x = 50;
-    y = 50;
+    sprite = new Sprite(walkFrames[0][0]);
+    sprite.setScale(1/32f);
+    sprite.setOriginCenter();
+    v = new Vector2();
+    iv = new Vector2();
+    x = _x;
+    y = _y;
     dir = Direction.DOWN;
   }
 
-  @Override
-  public void render() {
-    stateTime += Gdx.graphics.getDeltaTime();
+  public void setColMap(boolean[][] m) {
+    colMap = m;
+  }
+
+  public boolean keyDown(int keyCode) {
+    switch(keyCode) {
+      case Keys.LEFT:
+        dir = Direction.LEFT;
+        iv.x -= 1;
+        break;
+      case Keys.RIGHT:
+        dir = Direction.RIGHT;
+        iv.x += 1;
+        break;
+      case Keys.DOWN:
+        dir = Direction.DOWN;
+        iv.y -= 1;
+        break;
+      case Keys.UP:
+        dir = Direction.UP;
+        iv.y += 1;
+        break;
+      default:
+        return false;
+    }
+    v.set(iv);
+    v.nor().scl(SPEED);
+    return true;
+  }
+
+  public boolean keyUp(int keyCode) {
+    switch(keyCode) {
+      case Keys.LEFT:
+        iv.x += 1;
+        break;
+      case Keys.RIGHT:
+        iv.x -= 1;
+        break;
+      case Keys.DOWN:
+        iv.y += 1;
+        break;
+      case Keys.UP:
+        iv.y -= 1;
+        break;
+      default:
+        return false;
+    }
+    if (!iv.isZero()) {
+      if (iv.x > 0) { dir = Direction.RIGHT; }
+      if (iv.x < 0) { dir = Direction.LEFT; }
+      if (iv.y > 0) { dir = Direction.UP; }
+      if (iv.y < 0) { dir = Direction.DOWN; }
+    }
+    v.set(iv);
+    v.nor().scl(SPEED);
+    return true;
+  }
+
+  public void render(Camera cam) {
+    float d = Gdx.graphics.getDeltaTime();
+    stateTime += d;
+    float dx = v.x * d;
+    float dy = v.y * d;
+    // TODO(baptr): Cleanup and use character bounds.
+    if (x+dx < 0) { dx = 0; }
+    if (y+dy < 0) { dy = 0; }
+    if (colMap[(int)y][(int)(x+dx)]) { dx = 0; }
+    if (colMap[(int)(y+dy)][(int)x]) { dy = 0; }
+    x += dx;
+    y += dy;
+    sprite.setCenter(x, y);
     currentFrame = walkAnimations[dir.ordinal()].getKeyFrame(stateTime, true);
+    sprite.setRegion(currentFrame);
+    spriteBatch.setProjectionMatrix(cam.combined);
     spriteBatch.begin();
-    spriteBatch.draw(currentFrame, x, y);
+    sprite.draw(spriteBatch);
     spriteBatch.end();
   }
 }
--- core/src/net/baptr/roguish/Roguish.java
+++ core/src/net/baptr/roguish/Roguish.java
@@ -2,17 +2,20 @@ package net.baptr.roguish;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.InputMultiplexer;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-
-import com.badlogic.gdx.InputAdapter;
-import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
 
 import net.baptr.roguish.Player;
 
@@ -21,73 +24,68 @@ public class Roguish extends ApplicationAdapter {
   Texture img;
   TiledMap map;
   OrthogonalTiledMapRenderer renderer;
+  Viewport viewport;
   OrthographicCamera camera;
   Player player;
 
-  class OrthoCamController extends InputAdapter {
-    final OrthographicCamera camera;
-    final Vector3 curr = new Vector3();
-    final Vector3 last = new Vector3(-1, -1, -1);
-    final Vector3 delta = new Vector3();
-
-    public OrthoCamController (OrthographicCamera camera) {
-      this.camera = camera;
-    }
-
+  class InputController extends InputAdapter {
     @Override
-    public boolean touchDragged (int x, int y, int pointer) {
-      camera.unproject(curr.set(x, y, 0));
-      if (!(last.x == -1 && last.y == -1 && last.z == -1)) {
-        camera.unproject(delta.set(last.x, last.y, 0));
-        delta.sub(curr);
-        camera.position.add(delta.x, delta.y, 0);
+    public boolean keyDown(int keyCode) {
+      if (keyCode == Keys.ESCAPE) {
+        Gdx.app.exit();
+        return true;
       }
-      last.set(x, y, 0);
-      return false;
-    }
-
-    @Override
-    public boolean touchUp (int x, int y, int pointer, int button) {
-      last.set(-1, -1, -1);
       return false;
     }
   }
-  OrthoCamController cameraController;
+  InputController inputController;
 
   @Override
   public void create () {
-    batch = new SpriteBatch();
-    img = new Texture("rogueliketiles.png");
+    //batch = new SpriteBatch();
+    //img = new Texture("rogueliketiles.png");
     map = new TmxMapLoader().load("goblin_cave_test.tmx");
     renderer = new OrthogonalTiledMapRenderer(map, 1/32f);
 
-    float w = Gdx.graphics.getWidth();
-    float h = Gdx.graphics.getHeight();
-
     camera = new OrthographicCamera();
-    //camera.setToOrtho(false, (w / h) * 20, 20);
-    camera.setToOrtho(false, w/32f, h/32f);
-    camera.update();
+    camera.position.set(4, 9, 0);
+    viewport = new FitViewport(12, 10, camera);
 
-    cameraController = new OrthoCamController(camera);
-    Gdx.input.setInputProcessor(cameraController);
+    inputController = new InputController();
+    player = new Player(2, 8);
+    Gdx.input.setInputProcessor(new InputMultiplexer(inputController, player));
 
-    player = new Player();
+    player.setColMap(colMap());
   }
 
+  private boolean[][] colMap() {
+    TiledMapTileLayer l = (TiledMapTileLayer)(map.getLayers().get(0));
+    int h = l.getHeight();
+    int w = l.getWidth();
+    boolean[][] map = new boolean[h][w];
+    for (int y = 0; y < h; y++) {
+      for (int x = 0; x < w; x++) {
+        TiledMapTileLayer.Cell cell = l.getCell(x, y);
+        map[y][x] = (cell == null);
+      }
+    }
+    return map;
+  }
+
+  @Override
+  public void resize(int w, int h) {
+    viewport.update(w, h);
+  }
 
   @Override
   public void render () {
     Gdx.gl.glClearColor(32/255f, 29/255f, 32/255f, 1);
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+    camera.position.set(player.x, player.y, 0);
     camera.update();
     renderer.setView(camera);
-    renderer.render();
-    player.render();
-    /*
-    batch.begin();
-    batch.draw(img, 0, 0);
-    batch.end();
-    */
+    renderer.render(new int[]{0});
+    player.render(camera);
+    renderer.render(new int[]{1});
   }
 }
